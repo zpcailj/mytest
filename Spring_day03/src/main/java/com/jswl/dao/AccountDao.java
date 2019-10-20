c0cb4db31a1a18d7aee4cddb4e4ac3a2c2921848
78275744 <78275744@qq.com>
2019-10-20 15:59:54 +0800

    321

--- Spring_day03/src/main/java/com/jswl/dao/AccountDao.java
+++ Spring_day03/src/main/java/com/jswl/dao/AccountDao.java
@@ -1,6 +1,6 @@
 package com.jswl.dao;
 
 public interface AccountDao {
-	void outMoney(String from, Double money) ;
-	void inMoney(String to, Double money);
+	void outMoney(String from, Double money1111) ;
+	void inMoney(String to, Double money11111111);
 }
